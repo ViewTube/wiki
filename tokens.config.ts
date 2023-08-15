65606e6c467646a3e3697308549ea686099c276c
Mo <maurice@oeger.li>
2023-08-15 17:55:51 +0200

    Initial migration Co-authored-by: moisout <maurice@oeger.li>

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,18 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  color: {
+    primary: {
+      '50': '#fffaf7',
+      '100': '#fff4ef',
+      '200': '#ffe8de',
+      '300': '#ffd0bc',
+      '400': '#ffa179',
+      '500': '#ff7b3a',
+      '600': '#d16330',
+      '700': '#a34e27',
+      '800': '#75381d',
+      '900': '#3b1c0f'
+    }
+  }
 })
