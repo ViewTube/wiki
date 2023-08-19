4126ff0585e91a5d4030943a3198cc33668dae15
Maurice Oegerli <maurice@oeger.li>
2023-08-19 03:26:55 +0200

    ✨ Add logo and showcase components

---
+++ components/Logo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="logo">
+    <img class="icon" src="@/assets/icon.svg" alt="logo" />
+    <div class="logo-text">
+      <span>View</span>
+      <span class="colored">Tube</span>
+      <span class="lighter">Docs</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+:deep(.container) {
+  line-height: unset;
+}
+.logo {
+  display: flex;
+  gap: 4px;
+
+  .icon {
+    width: 2rem;
+    height: 2rem;
+  }
+
+  .logo-text {
+    font-family: "expletus-bold", "Open Sans", sans-serif;
+    font-size: 1.5rem;
+    color: #d2d2d2;
+
+    .colored {
+      color: transparent;
+      background-image: linear-gradient(
+        53deg,
+        rgba(241, 87, 10, 1) 0%,
+        rgba(224, 140, 112, 1) 33%,
+        rgba(89, 193, 186, 1) 78%,
+        rgba(0, 212, 255, 1) 100%
+      );
+      background-clip: text;
+    }
+
+    .lighter {
+      color: #ebebeb;
+      margin: 0 0 0 0.5rem;
+    }
+  }
+}
+</style>
---
+++ components/content/Showcase.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+const titleWidth = () => `${Math.floor(40 + Math.random() * (100 - 40 + 1))}%`;
+</script>
+
+<template>
+  <div>
+    <div class="showcase">
+      <div class="video" v-for="i in 6" :key="i">
+        <div class="image-outer">
+          <div class="image" />
+        </div>
+        <div class="title-outer">
+          <div class="title" :style="{ width: titleWidth() }" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.showcase {
+  background-image: linear-gradient(45deg, #000000e8 0%, #00000000 60%),
+    radial-gradient(
+      100% 100% at 0% 50%,
+      #f1570a 21%,
+      rgba(241, 87, 10, 0.75) 40.5%,
+      rgba(241, 87, 10, 0.5) 60%,
+      rgba(241, 87, 10, 0) 99%
+    ),
+    linear-gradient(180deg, #ffffb5 0%, #f6d860 14%, #e08c70 31%, #e08c70 51%, #59c1ba 77%, #00d4ff 94%);
+  width: 100%;
+  // aspect-ratio: 16/9;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  box-sizing: border-box;
+  overflow: hidden;
+
+  .video {
+    display: flex;
+    flex-direction: column;
+
+    .image-outer {
+      padding: 7.5px 7.5px 0 7.5px;
+      overflow: hidden;
+      width: 100%;
+      aspect-ratio: 16/9;
+      box-sizing: border-box;
+
+      .image {
+        height: 100%;
+        width: 100%;
+        box-shadow: 0 0 0 50px #0c0c0d;
+        border-radius: 8px;
+      }
+    }
+    .title-outer {
+      width: 100%;
+      height: 30px;
+      padding: 10px 7.5px 7.5px 7.5px;
+      box-sizing: border-box;
+      overflow: hidden;
+
+      .title {
+        height: 100%;
+        box-shadow: 0 0 0 200px #0c0c0d;
+        border-radius: 8px;
+      }
+    }
+  }
+}
+</style>
