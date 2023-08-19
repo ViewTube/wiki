7bd4fbf10b8314850b267fdd82085af2688ca6e8
Mo <maurice@oeger.li>
2023-08-19 03:24:31 +0200

    Update docs

--- app.config.ts
+++ app.config.ts
@@ -1,38 +1,54 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: 'ViewTube Wiki',
+    description: 'The open source, privacy-conscious way to enjoy your favorite YouTube content. ',
+    image: '/viewtube_cover.png',
+
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      twitter: '',
+      github: 'viewtube/viewtube',
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
         href: 'https://nuxt.com'
       }
     },
+
     github: {
-      dir: '.starters/default/content',
+      dir: 'content',
       branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
+      repo: 'wiki',
+      owner: 'viewtube',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
-      padded: true,
-      fluid: true
+      padded: false,
+      fluid: false
     },
+
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
-      fluid: true
-    }
+      fluid: false,
+      title: 'ViewTube Wiki'
+    },
+
+    footer: {
+      credits: {
+        icon: 'mdi:play',
+        text: 'Visit ViewTube',
+        href: 'https://viewtube.io'
+      }
+    },
+
+    titleTemplate: '%s · ViewTube Wiki'
   }
 })
